bc5263b7839ade80c434eb8e0e14f46ba3d6b3b6
Brian DuSell <bdusell@gmail.com>
2024-01-09 19:34:46 +0100

    Remove unused Sublayer class.

--- src/stack_attention/sublayer.py
+++ src/stack_attention/sublayer.py
@@ -9,25 +9,6 @@ from torch_unidirectional import (
     DropoutUnidirectional
 )
 
-class Sublayer:
-    """A sublayer of a transformer encoder. Adds layer norm, dropout, and
-    residual connections to a sublayer function."""
-
-    def __init__(self,
-        sublayer_func: torch.nn.Module,
-        d_model: int,
-        dropout: Optional[float]
-    ):
-        super().__init__()
-        self.sublayer_func = sublayer_func
-        self.layer_norm = torch.nn.LayerNorm((d_model,))
-        self.dropout = torch.nn.Dropout(dropout) if dropout else torch.nn.Identity()
-
-    def forward(self, input_sequence, *args, **kwargs):
-        # input_sequence : batch_size x sequence_length x d_model
-        # return : batch_size x sequence_length x d_model
-        return input_sequence + self.dropout(self.sublayer_func(self.layer_norm(input_sequence), *args, **kwargs))
-
 def get_unidirectional_sublayer(
     sublayer_func: Unidirectional,
     d_model: int,
